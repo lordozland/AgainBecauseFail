38ff01f718b947647cfc207210d0e5d9251cb99c
William Crews <crewswh@gmail.com>
2021-04-15 23:38:23 -0400

    fixing

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,29 @@
 var imageContainer = document.querySelector(".img-container");
-var C1 = 
+var C1 = document.querySelector("h2")
 
-// Listen for any clicks within the img-container div
+
 imageContainer.addEventListener("click", function(event) {
   var element = event.target;
 
-  // Check if the clicked element was an image
   if (element.matches("img")) {
-    // Get the current value of the image's data-state attribute
     var state = element.getAttribute("data-state");
 
     if (state === "still") {
-      // Change the data-state attribute's value
-      // There are two different ways this attribute can be set
       element.dataset.state = "animate";
       element.setAttribute("data-state", "animate");
-
-      // Update the image's source to the string being stored in the data-animate attribute
       element.setAttribute("src", element.dataset.animate);
     } else {
-      // Change the attributes back to their non-animated values
       element.dataset.state = "still";
       element.setAttribute("src", element.dataset.still);
     }
   }
 });
+
+//  Listen for any clicks within the img-container div
+// Get the current value of the image's data-state attribute
+      // Change the data-state attribute's value
+      // There are two different ways this attribute can be set
+      // Update the image's source to the string being stored in the data-animate attribute
+      // Change the attributes back to their non-animated values
+// I want the background to change with each click and the pictures to enlage to the .gifs when clicked.  
+// When all four are clicked, Toto's Hold the Line Plays
